234e6d050bdeb3a7c34e874203009b282deaffd1
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-12-13 15:13:56 +0530

    router install

--- crud-no-api/src/index.js
+++ crud-no-api/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './components/Home';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {} from "react-"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
